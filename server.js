35dc7ea88fb5112277783dfa863506963d8fbf1f
avivarendse <avivarendse8@gmail.com>
2026-08-18 15:53:46 +0200

    Create MVC, dashboard.js, employees.js, .env, server.js, employeeController.js, dashboardController.js,database.js, tested both APIs

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import employeeRoutes from './routes/employees.js';
+import dashboardRoutes from './routes/dashboard.js';
 
 dotenv.config();
 
@@ -8,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/employees', employeeRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
