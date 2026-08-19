0ef055c2818f67398423a5dabad078b085fde09c
avivarendse <avivarendse8@gmail.com>
2026-08-19 10:02:51 +0200

    Changed port number from 3000 to 4000

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api/employees', employeeRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
