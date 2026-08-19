e5fb499152ce4053e10a9023044299de6706b6b0
saajidahabrahams <saajidah2007@gamil.com>
2026-08-19 09:05:08 +0200

    Updated port number

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/performance", performanceRoutes);
 app.use("/api/reports", reportsRoutes);
 app.use("/api/settings", settingsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`HRFlow backend running on port ${PORT}`);
