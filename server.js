7dd14bbfb55e9a772f8da03082603c9e72dd27ee
Zaarah Okkers <zaarahokkers7@gmail.com>
2026-08-20 14:28:28 +0200

    Server local host port changed to 4000

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['http://localhost:4000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
     credentials: true
 }));
 app.use(express.json());
